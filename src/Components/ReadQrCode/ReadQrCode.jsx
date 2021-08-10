05d0c10399153c44bc464b9857d71bb1e703c418
camicasii <camicasii@hotmail.com>
2021-08-10 09:41:05 -0400

    update

--- src/Components/ReadQrCode/ReadQrCode.jsx
+++ src/Components/ReadQrCode/ReadQrCode.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Grid } from "@material-ui/core";
 import { useStyles } from "./style";
-import QrReader from "react-qr-scanner";
+import QrReader from "react-qr-reader";
 
 import ScanReceive from "./ScanReceive";
 
