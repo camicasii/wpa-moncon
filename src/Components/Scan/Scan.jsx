af96b37261194c1c3252e704d02bd16cc4e55807
camicasii <camicasii@hotmail.com>
2021-08-10 22:33:17 -0400

    1111123

--- src/Components/Scan/Scan.jsx
+++ src/Components/Scan/Scan.jsx
@@ -63,7 +63,7 @@ const [display, setDisplay] = useState(false)
   };
 
   useEffect(() => {
-    socketRef.current = io(process.env.REACT_APP_MONCON_API_BASE_URL);    
+    socketRef.current = io(process.env.REACT_APP_MONCON_API_BASE_URL_SOCKET);    
     socketRef.current.on('connect', () => {        
       console.log(socketRef.current.id);
       
